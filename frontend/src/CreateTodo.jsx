815bf4e36fa8f8104646f8ab5637d5eebef53796
mac <mac@192.168.1.10>
2024-05-03 23:11:59 +0530

    remove everything starting from 0

--- frontend/src/CreateTodo.jsx
+++
@@ -1,7 +0,0 @@
-export function CreateTodo(){
-    return <div>
-        <input type="text" name="title" id="" placeholder="title"/> <br/>
-        <input type="text" name="description" id="" placeholder="description"/> <br/>
-        <button>Add a todo</button>
-        </div>
-}
